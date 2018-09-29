a83fbc009ca2f0a1a9a86083cbc1748172b0cacd
muralivijay <muralidharan9845@gmail.com>
2018-09-29 15:54:27 +0530

    sakura: drop consumerir HIDL
    * we use prebuilt

--- device.mk
+++ device.mk
@@ -29,10 +29,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml
 
-# Consumerir
-PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-service.xiaomi_sakura
-
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.sakura.rc \
