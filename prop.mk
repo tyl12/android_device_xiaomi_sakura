331efd839d6877019baf932d04e6593015d83943
Rama Bondan Prakoso <ramanarubp@gmail.com>
2019-01-13 01:47:15 +0700

    Revert "sakura: Enable navbar via prop"
    This reverts commit ff73d7516dd863e4e20c9fab534eae951d08e291.

--- prop.mk
+++ prop.mk
@@ -2,10 +2,6 @@
 # system.prop for vince
 #
 
-# Navbar
-PRODUCT_PROPERTY_OVERRIDES += \
-qemu.hw.mainkeys=0
-
 # Vendor Security Patch Level
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.security_patch=2018-12-05
